f13e040bf42893ba41f6a7186861fbea14cc7706
ryandelongchamps <89552468+ryandelongchamps@users.noreply.github.com>
2021-12-14 10:37:43 -0600

    start project, create models and admin

---
+++ pizzas/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+class Comment(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
